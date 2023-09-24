754c9d316467117dd6af1465e7e79ad39815e37e
Aniii7 <94300494+Aniii7@users.noreply.github.com>
2023-09-24 21:06:00 +0530

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -8,12 +8,7 @@ window.onload = function() {
 }
 
 function setGame() {
-    // board = [
-    //     [2, 2, 2, 2],
-    //     [2, 2, 2, 2],
-    //     [4, 4, 8, 8],
-    //     [4, 4, 8, 8]
-    // ];
+   
 
     board = [
         [0, 0, 0, 0],
@@ -31,7 +26,7 @@ function setGame() {
             document.getElementById("board").append(tile);
         }
     }
-    //create 2 to begin the game
+
     setTwo();
     setTwo();
 
@@ -39,7 +34,7 @@ function setGame() {
 
 function updateTile(tile, num) {
     tile.innerText = "";
-    tile.classList.value = ""; //clear the classList
+    tile.classList.value = "";
     tile.classList.add("tile");
     if (num > 0) {
         tile.innerText = num.toString();
@@ -73,11 +68,11 @@ document.addEventListener('keyup', (e) => {
 })
 
 function filterZero(row){
-    return row.filter(num => num != 0); //create new array of all nums != 0
+    return row.filter(num => num != 0); 
 }
 
 function slide(row) {
-    //[0, 2, 2, 2] 
+  
     row = filterZero(row); //[2, 2, 2]
     for (let i = 0; i < row.length-1; i++){
         if (row[i] == row[i+1]) {
@@ -85,12 +80,12 @@ function slide(row) {
             row[i+1] = 0;
             score += row[i];
         }
-    } //[4, 0, 2]
-    row = filterZero(row); //[4, 2]
-    //add zeroes
+    } 
+    row = filterZero(row);
+ 
     while (row.length < columns) {
         row.push(0);
-    } //[4, 2, 0, 0]
+    } 
     return row;
 }
 
@@ -112,7 +107,7 @@ function slideRight() {
         let row = board[r];         //[0, 2, 2, 2]
         row.reverse();              //[2, 2, 2, 0]
         row = slide(row)            //[4, 2, 0, 0]
-        board[r] = row.reverse();   //[0, 0, 2, 4];
+        board[r] = row.reverse();   //[0, 0, 2, 4]
         for (let c = 0; c < columns; c++){
             let tile = document.getElementById(r.toString() + "-" + c.toString());
             let num = board[r][c];
@@ -160,7 +155,7 @@ function setTwo() {
     }
     let found = false;
     while (!found) {
-        //find random row and column to place a 2 in
+        
         let r = Math.floor(Math.random() * rows);
         let c = Math.floor(Math.random() * columns);
         if (board[r][c] == 0) {
@@ -177,7 +172,7 @@ function hasEmptyTile() {
     let count = 0;
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
-            if (board[r][c] == 0) { //at least one zero in the board
+            if (board[r][c] == 0) { 
                 return true;
             }
         }
